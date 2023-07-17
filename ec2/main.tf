5da37052c5e34c6d160969045ee822e4322ece27
Matias Vera <matias@bajacloud.io>
2023-07-17 14:31:06 -0400

    first commit

---
+++ ec2/main.tf
@@ -0,0 +1,23 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.16"
+    }
+  }
+
+  required_version = ">= 1.2.0"
+}
+
+provider "aws" {
+  region  = "us-east-1"
+}
+
+resource "aws_instance" "app_server" {
+  ami           = "ami-06ca3ca175f37dd66"
+  instance_type = "t2.large"
+
+  tags = {
+    Name = "driftdetectionexample"
+  }
+}
